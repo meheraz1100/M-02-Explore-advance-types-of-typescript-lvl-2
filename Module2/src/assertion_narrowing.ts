41f72f2df5ef833c973645978429dfb85649846c
meheraz1100 <meheraz1100@gmail.com>
2024-11-01 22:23:29 +0600

    Type assertion / type narrowing

--- Module2/src/assertion_narrowing.ts
+++ Module2/src/assertion_narrowing.ts
@@ -5,12 +5,35 @@
 
 let anyThing : any;
 
+anyThing = 'Mosaiyeb Islam Meheraz';
+
+anyThing = 222;
+
+// (anyThing as number)
+
+const KgToGm = (value : string | number) : string | number | undefined =>{
+    if(typeof value === 'string'){
+        const convertedValue = parseFloat(value) * 1000;
+        return `The Converted Value is ${convertedValue}`
+    }
+    if(typeof value === 'number'){
+        return value * 1000;
+    }
+}
 
 
+const result1 = KgToGm(1000) as number;
+const result2 = KgToGm('1000') as string;
 
+type customError = {
+    message : string
+}
 
+try {
 
-
+}catch(error){
+    console.log((error as customError).message);
+}
 
 
 
